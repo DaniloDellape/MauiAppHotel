20b5c375b4d0f3caae8658eb8c54a7145ad4b1c9
DaniloDellape <danilodellape@gmail.com>
2025-11-09 18:47:31 -0300

    atualização dos campos Suíte e Estadia

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,35 @@
 ﻿
+using MauiAppHotel.Models;
+using System.Security.Cryptography.X509Certificates;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Simples",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
